f9c078022f2939a2b177d851cf15db7f400f9bdb
MinexAutomation <minex.automation@gmail.com>
2019-12-14 03:47:30 -0800

    Added AddXServies() service aggregation service collection extension for default Octokit services.

---
+++ source/R5T.Goteborg.Octokit.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia.Extensions;
+
+
+namespace R5T.Goteborg.Octokit.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDefaultOctokitGitHubOperatorServices<TProductHeaderValueProvider>(this IServiceCollection services)
+            where TProductHeaderValueProvider: class, IProductHeaderValueProvider
+        {
+            services
+                .AddSingleton<IGitHubOperator, GitHubOperator>()
+                .AddSingleton<IGitHubClientProvider, DefaultGitHubClientProvider>()
+                .AddSingleton<IAuthenticatedGitHubClientProvider, DefaultAuthenticatedGitHubClientProvider>()
+                .AddSingleton<IUnauthenticatedGitHubClientProvider, DefaultUnauthenticatedGitHubClientProvider>()
+                .AddSingleton<IProductHeaderValueProvider, TProductHeaderValueProvider>();
+                ;
+
+            return services;
+        }
+    }
+}
